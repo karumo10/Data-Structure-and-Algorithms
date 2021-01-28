9e1489f0f77f2e4ac4832954b51cdcf386aaffe2
karumo10 <leo_yxzhou22@sjtu.edu.cn>
2021-01-28 14:22:15 +0800

    integer inddefinite test

--- CSAPP/inf_nan.cpp
+++ CSAPP/inf_nan.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#define POS_INFINITY 1.8e308
+#define POS_INFINITY 1.8e308 // overflow
 #define NEG_INFINITY -1.8e308
 #define NEG_ZERO 1/-1.8e308
 #define NAN POS_INFINITY+NEG_INFINITY
 using namespace std;
 int main() {
     double a = 1.7e308;
-    cout << NEG_INFINITY << NEG_ZERO << NAN;
+    cout << NEG_INFINITY << NEG_ZERO << NAN << endl;
+    cout << (int)a << endl; // overflow, intel cpu set 0x80000000 (1000....00) as the so-called "integer indefinite" to express those values that overflow by a cast from double/float to int
     return 0;
 }
